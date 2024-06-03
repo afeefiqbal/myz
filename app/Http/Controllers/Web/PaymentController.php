386d17a01728c01a59b45311df1ab1d2aa9b7737
afeeflilac <afeef@lilacinfotech.com>
2024-06-03 17:13:59 +0530

    #changes in payment controller

--- app/Http/Controllers/Web/PaymentController.php
+++ app/Http/Controllers/Web/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderProduct;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Validator;
@@ -15,22 +16,36 @@ use Stripe\StripeClient;
 
 class PaymentController extends Controller
 {
-  
+
     public function payment($order_id, $amount, $billingParams)
     {
-        dd($order_id, $amount, $billingParams);
 
+        $orderProducts = OrderProduct::with('productData')->where('order_id',$order_id)->get();
+        $products = [];
+        foreach($orderProducts as $prd){
+            $products []= [
+                'name' => $prd->productData->title,
+                'price' => $prd->productData->price
+            ];
+        }
+
+        $order = Order::where('id', $order_id)->first();
+
+        $phone_number = str_replace('/', '', $billingParams['phone_number']);
+        $phone_number = str_replace('+', '', $phone_number);
+        $billingParams['phone'] = $phone_number;
+        $billingParams['currency'] =  $order->currency;
+        $currency = $order->currency;
+        $description = 'Order of : ' . $billingParams['first_name'] . ' with order id ' . $order_id;
         Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
         $session = Session::create([
             'line_items'  => [
                 [
                     'price_data' => [
-                        'currency'     => 'aed',
-                        'product_data' => [
-                            'name' => 'T-shirt',
-                        ],
-                        'unit_amount'  => 500,
+                        'currency'     => $currency,
+                        'product_data' => $products,
+                        'unit_amount'  => $amount,
                     ],
                     'quantity'   => 1,
                 ],
@@ -39,7 +54,7 @@ class PaymentController extends Controller
             'success_url' => route('payment.success'),
             'cancel_url'  => route('payment.failure'),
         ]);
-        
+
         return ($session->url);
 
     }
@@ -48,7 +63,7 @@ class PaymentController extends Controller
     {
 
         $response = app(CartController::class)->order_success(1);
-        
+
         return redirect(url($response['data']));
         // Handle payment cancellation
         // You can implement this method according to your business logic
@@ -57,7 +72,7 @@ class PaymentController extends Controller
     {
 
         $response = app(CartController::class)->order_payment_cancelled(1);
-        
+
         return redirect(url($response['data']));
         // Handle payment cancellation
         // You can implement this method according to your business logic
@@ -65,7 +80,7 @@ class PaymentController extends Controller
 
     public function paymentDeclined(Request $request)
     {
-        dd('s');
+   
         $response = app(CartController::class)->order_payment_failed(1);
 
         return redirect(url($response['data']));
